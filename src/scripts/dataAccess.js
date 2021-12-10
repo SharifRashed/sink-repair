841120f75db1b41527bf95607f6e4871a7dc9644
sharifrashed <sharif.rashed26@gmail.com>
2021-12-10 14:21:48 -0600

    finshed project and made delete button work

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,4 +1,4 @@
-// import { mainContainer } from "./main.js"
+const mainContainer = document.querySelector("#container")
 
 const applicationState = {
 
@@ -44,4 +44,13 @@ export const sendRequest = (userServiceRequest) => {
 
 export const getRequests = () => {
     return applicationState.requests.map(request => ({ ...request }))
+}
+
+export const deleteRequest = (id) => {
+    return fetch(`${API}/requests/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
 }
